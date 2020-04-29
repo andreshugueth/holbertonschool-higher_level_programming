5309471e44b41af52f3334ed5fcff2b4092b7f97
Andres Hugueth <andreshugueth@gmail.com>
2020-04-29 00:45:05 +0000

    fixing identation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,13 +4,13 @@ number = random.randint(-10000, 10000)
 x = abs(number) % 10
 
 if x <= 0:
-	x = x * -1
+        x = x * -1
 
 if x > 5:
-	str = "greater than 5"
+        str = "greater than 5"
 elif x == 0:
-	str = "0"
+        str = "0"
 else:
-	str = "less than 6 and not 0"
+        str = "less than 6 and not 0"
 
 print("Last digit of {:d} is {:d} and is {:s}".format(number, x, str))
