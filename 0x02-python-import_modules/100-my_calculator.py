2faa50b2585edb16ea1986880037d7b13a212c8c
Andres Hugueth <andreshugueth@gmail.com>
2020-05-01 02:03:50 +0000

    fixed importation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
+from sys import argv
+from calculator_1 import add, sub, mul, div
 
-    argv = argv[1:]
+argv = argv[1:]
+if __name__ == "__main__":
     if len(argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
