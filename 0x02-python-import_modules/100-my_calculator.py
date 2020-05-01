7ccc4a0448b6e5d5043c2ddc27d207f197d68695
Andres Hugueth <andreshugueth@gmail.com>
2020-05-01 02:08:34 +0000

    fixed result of operation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-
-argv = argv[1:]
 if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+
+    argv = argv[1:]
+
     if len(argv) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     a = int(argv[0])
     operator = argv[1]
     b = int(argv[2])
@@ -23,4 +25,4 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     
-    print("{} {} {}".format(a, operator, b, operation))
+    print("{} {} {} = {}".format(a, operator, b, operation))
