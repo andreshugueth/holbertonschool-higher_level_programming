c80a369e71aa37a3dea76302188b769145871780
Andres Hugueth <andreshugueth@gmail.com>
2020-05-05 18:38:10 -0500

    new list mod

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    list_copy = my_list[:]
+    if idx < 0:
+        return list_copy
+    count = 0
+    for n in list_copy:
+        count += 1
+    if idx < count:
+        list_copy[idx] = element
+        return list_copy
+    else:
+        return list_copy
