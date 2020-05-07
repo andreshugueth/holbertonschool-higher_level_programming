b19cbd4f88d659309cbed81fb06c1e476ee71224
Andres Hugueth <andreshugueth@gmail.com>
2020-05-07 17:30:08 -0500

    replace value of list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i, elem in enumerate(new_list):
+        if elem == search:
+            new_list[i] = replace
+    return new_list
