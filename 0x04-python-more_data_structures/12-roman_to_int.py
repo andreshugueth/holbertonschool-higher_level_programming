e24de4c910e2dfd2d6d2c543191629e2f7c196a8
Andres Hugueth <andreshugueth@gmail.com>
2020-05-07 23:05:01 -0500

    pep8 fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,9 @@
 
 
 def roman_to_int(roman_string):
-    if type(roman_string)  != str or roman_string is None:
+    if type(roman_string) != str or roman_string is None:
         return 0
-    numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
+    numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     conv = 0
     prev = 0
     for pos in roman_string:
