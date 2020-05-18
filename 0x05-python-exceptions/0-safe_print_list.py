996936a94b0cfcb9cf80587e4f9595fe76d6c226
Andres Hugueth <andreshugueth@gmail.com>
2020-05-18 13:09:14 -0500

    0-safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for index, item in enumerate(my_list):
+            if index == x:
+                break
+            print("{}".format(item), end='')
+            count += 1
+        print()
+        return count
+    except:
+        print("Something went wrong")
