9a6df9ff33f9b6c90561249d46b3f274a6df9315
andreshugueth <andreshugueth@gmail.com>
2020-05-19 01:14:30 -0500

    6th task comment

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-
+# Hello world 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for index in range(0, x):
@@ -11,4 +11,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
     print()
-    return count
+    return count    
