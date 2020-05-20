3111b25d63656a0a628d3f88a2f056bd84f754e0
andreshugueth <andreshugueth@gmail.com>
2020-05-19 21:55:46 -0500

    spaces

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,6 +13,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """area of a square"""
         return self.__size ** 2
