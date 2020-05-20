0f22044bfd5784eec0c42fb02cf5ec87b40a9be4
andreshugueth <andreshugueth@gmail.com>
2020-05-19 19:40:56 -0500

    fixed comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Square size"""
+"""Square class"""
 
 
 class Square:
     def __init__(self, size):
-        """Represent a square
+        """define a square size
         Arguments:
             size: size of the square
         """
