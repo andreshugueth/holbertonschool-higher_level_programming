cde440484e539343aaf3a2d69ed85441c8522818
andreshugueth <andreshugueth@gmail.com>
2020-05-19 23:56:28 -0500

    fixed identation2

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,68 +1,68 @@
 #!/usr/bin/python3
-"""square class"""
+"""Square class"""
 
 
 class Square:
-    """Represent a square
+    """define size
     Arguments:
         size: size of a square
     """
     def __init__(self, size=0, position=(0, 0)):
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        if type(position) is not tuple or len(position) != 2:
+        elif type(position) is not tuple or len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[0]) is not int or position[0] < 0:
+        elif type(position[0]) is not int or position[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[1]) is not int or position[1] < 0:
+        elif type(position[1]) is not int or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+
         self.__size = size
         self.__position = position
 
     def area(self):
-        """Represent the area"""
+        """area of square"""
         return self.__size ** 2
 
     def my_print(self):
-        """Represent printing"""
-        if self.__size < 0:
-            print()
-        else:
-            if self.__position[1] > 0:
-                for newline in range(self.__position[1]):
+        if self.size > 0:
+            if self.position[1] > 0:
+                for newline in range(self.position[1]):
                     print("")
-            for row in range(self.__size):
-                if self.__position[0] > 0:
-                    for spaces in range(self.__position[0]):
+            for row in range(self.size):
+                if self.position[0] > 0:
+                    for spaces in range(self.position[0]):
                         print(" ", end="")
-                for collumn in range(self.__size):
+                for collumn in range(self.size):
                     print("#", end="")
-                print()
+                print("")
+        else:
+            print("")
 
     @property
     def size(self):
-        """Represent the size"""
+        """getter function"""
         return self.__size
 
-    @size.getter
+    @size.setter
     def size(self, value):
-        """Represent size setter"""
+        """setter size function"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
     def position(self):
-        """Represent position"""
+        """positional value"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Represent pos setter"""
+        """position setter"""
         if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value) is not int or value[0] < 0:
