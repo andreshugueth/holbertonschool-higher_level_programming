30a1eb6f08661ec328043edb035aa8cde31bfa64
andreshugueth <andreshugueth@gmail.com>
2020-05-19 19:37:48 -0500

    first task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square size"""
+
+
+class Square:
+    def __init__(self, size):
+        """Represent a square
+        Arguments:
+            size: size of the square
+        """
+        self.__size = size
