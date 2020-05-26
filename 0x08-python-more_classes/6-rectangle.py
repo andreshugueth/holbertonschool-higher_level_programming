de314fc54246233a549af63e95cdc9240d9dd82d
andreshugueth <andreshugueth@gmail.com>
2020-05-25 19:20:44 -0500

    fixing docstring

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,9 +3,9 @@
 
 
 class Rectangle:
+    """Represent a rectangle"""
     # count instances
     number_of_instances = 0
-    """Represent a rectangle"""
     def __init__(self, width=0, height=0):
         """Initialization of rectangle class
         Keyword Arguments:
