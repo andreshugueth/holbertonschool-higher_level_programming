4aced40ce9ca733c47e149f57340f398b835f1f4
andreshugueth <andreshugueth@gmail.com>
2020-06-02 18:38:04 -0500

    docstring

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Function that returns dir of a object
+"""
 
 
 def lookup(obj):
+    """dir(obj)
+
+    Arguments:
+        obj {any} -- [type of object]
+
+    Returns:
+        [dir(obj)] -- attributes and methods of an obj
+    """
     return dir(obj)
