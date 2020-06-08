ebb61946163123a9ff3a4863ae32f2a62b873694
andreshugueth <andreshugueth@gmail.com>
2020-06-08 17:12:55 -0500

    3rd task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,63 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor class"""
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """width setter"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """height setter"""
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
+
+    @property
+    def x(self):
+        """x dimension getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """x dimension setter"""
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        elif (x < 0):
+            raise ValueError("x must be >= 0")
         self.__x = x
+
+    @property
+    def y(self):
+        """y dimension getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """y dimension setter"""
+        if type(y) != int:
+            raise TypeError("y mus be an integer")
+        elif (y < 0):
+            raise ValueError("y must be >= 0")
         self.__y = y
