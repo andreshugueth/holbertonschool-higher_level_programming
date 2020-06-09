7d3b82db578a9597a274ba74788487e50530736d
andreshugueth <andreshugueth@gmail.com>
2020-06-09 01:12:02 -0500

    handling x and y display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,8 +75,10 @@ class Rectangle(Base):
 
     def display(self):
         """Display rectangle"""
+        for ypos in range(self.__y):
+            print()
         for elem in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """str representation class"""
