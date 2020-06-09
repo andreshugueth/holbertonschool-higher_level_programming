f440efd7fdd7988da88c74a1cfc1697ca24cd0d4
andreshugueth <andreshugueth@gmail.com>
2020-06-09 11:57:57 -0500

    dict repres square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -50,3 +50,12 @@ class Square(Rectangle):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """Dictionary representation of square"""
+        new_dict = {}
+        new_dict["id"] = self.id
+        new_dict["x"] = self.x
+        new_dict["size"] = self.size
+        new_dict["y"] = self.y
+        return new_dict
