40061af2b0a470aed831bb6ca07f3674e8506006
andreshugueth <andreshugueth@gmail.com>
2020-06-09 19:47:29 -0500

    fixing id 10th task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
         self.size = size
 
     @property
