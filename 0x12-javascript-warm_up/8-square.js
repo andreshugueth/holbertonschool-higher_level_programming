a5731c903b947e9d0512e117a308c12cff0fa960
andreshugueth <andreshugueth@gmail.com>
2020-09-15 22:25:50 -0500

    javascript task 8 fixed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 // Script that prints a square
 const size = parseInt(process.argv[2]);
 if (isNaN(size)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   if (size > 0) {
     for (let i = 0; i < size; i++) {
