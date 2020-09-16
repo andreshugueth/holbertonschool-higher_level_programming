993bbe069f121d88609e6df422f23ea1b3b10c37
andreshugueth <andreshugueth@gmail.com>
2020-09-15 22:36:14 -0500

    javascript task 9 fixed

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Script that prints the addition of 2 integers
 function add (a, b) {
-  const num_1 = parseInt(a);
-  const num_2 = parseInt(b);
-  if (isNaN(num_1) || isNaN(num_2)) {
+  const num1 = parseInt(a);
+  const num2 = parseInt(b);
+  if (isNaN(num1) || isNaN(num2)) {
     return NaN;
   } else {
-    return num_1 + num_2;
+    return num1 + num2;
   }
 }
 console.log(add(process.argv[2], process.argv[3]));
