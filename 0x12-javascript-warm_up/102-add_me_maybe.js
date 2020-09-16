aa69095fa63a819d58b8acc6acd1cb0b1f6929c3
andreshugueth <andreshugueth@gmail.com>
2020-09-15 23:30:38 -0500

    javascript task 16

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-/* Write a function that increments and calls a function.
-The function must be visible from outside */
-const addMeMaybe = function (x, cb) {
-  cb(x + 1);
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
 };
-module.exports = { addMeMaybe };
