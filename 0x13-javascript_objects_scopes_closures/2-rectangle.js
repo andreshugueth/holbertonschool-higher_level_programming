80b85348e66dd72c6b6e3402a91ffc27488a50b9
andreshugueth <andreshugueth@gmail.com>
2020-09-21 20:32:42 -0500

    js objs task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * A class Rectangle that defines a rectangle:
+ * -------------------------------------------
+ * w: width of a rectangle.
+ * h: height of a rectangle.
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
