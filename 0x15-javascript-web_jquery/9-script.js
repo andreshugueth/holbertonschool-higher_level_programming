dc56e02eb8021187a7bf67f43faf188b5a7ad4ff
andreshugueth <andreshugueth@gmail.com>
2020-09-24 16:35:38 -0500

    js web jquery tk 9

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,8 @@
+const $ = window.$;
+$(function () {
+    $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, textStatus) {
+        if (textStatus === 'success') {
+            $('#hello').text(data.hello);
+        }
+    });
+});
