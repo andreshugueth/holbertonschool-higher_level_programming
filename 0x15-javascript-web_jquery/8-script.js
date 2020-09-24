025f62b99f89dca90ce3326c32bd938862243f39
andreshugueth <andreshugueth@gmail.com>
2020-09-24 16:28:03 -0500

    js web jquery tk 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+const $ = window.$;
+$(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data, textStatus) {
+    if (textStatus === 'success') {
+      const titles = data.results;
+      for (const i in titles) {
+        $('#list_movies').append(`<li> ${titles[i].title} </li>`);
+      }
+    }
+  });
+});
